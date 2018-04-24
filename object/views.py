f203f44b2ab4119d43d26922f0cb86d684f91821
aliyun-panyue <aliyun-panyue@aliyun.com>
2018-04-24 09:55:36 +0800

    daily replay v3 migrate

--- object/views.py
+++ object/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+
 from .models import UserObject,Objects
 from .form import UserObjectForm
 
